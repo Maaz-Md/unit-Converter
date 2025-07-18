f54ca7e779bc3de79f2c9511a0aa53d4cd8f620e
MaazMd <muazbellary111@gmail.com>
2025-07-18 11:06:06 +0530

    completed length form

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LengthConverter from "./components/lengthConverter";
+
+function App() {
+  return (
+    <div>
+      <h1>Unit Converter</h1>
+      <LengthConverter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/lengthConverter.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react"
+
+const lengthConversions = {
+  mm: 0.001,
+  cm: 0.01,
+  m: 1,
+  km: 1000,
+  inch: 0.0254,
+  foot: 0.3048,
+  yard: 0.9144,
+  mile: 1609.34
+}
+
+
+
+export default function LengthConverter() {
+
+  const [num, setNum] = useState("")
+  const [fromUnit, setFromUnit] = useState("m")
+  const [toUnit, setToUnit] = useState("km")
+  const [result, setResult] = useState(null)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const ValueInMeters = Number(num) * lengthConversions[fromUnit]
+    const finalValue = ValueInMeters / lengthConversions[toUnit]
+    setResult(finalValue)
+    setIsSubmitted(true)
+  }
+
+  const handlerReset = () => {
+      setNum("")
+      setFromUnit("m")
+      setToUnit("km")
+      setResult(null)
+      setIsSubmitted(false)
+  }
+
+  if (isSubmitted) {
+    return (
+      <>
+        <p>Result of your calculation</p>
+        <h2>{num} {fromUnit} = {result} {toUnit}</h2>
+        <button type="button" onClick={handlerReset}>Reset</button>
+      </>
+    )
+  }
+  
+    return (
+        <form onSubmit={handleSubmit}>
+          <h2>Length</h2>
+          <label for="length">Enter the length to convert</label>
+            <input 
+              type="number"
+              value={num}
+              onChange={(e) => (setNum(e.target.value))}
+              min="0"
+              step="0.01"
+              placeholder="Enter a number"
+              id="length"
+              required
+              autoFocus
+            /><br/>
+          <label for="fromUnits">Unit to convert from</label>
+            <select id="fromUnits" value={fromUnit} onChange={(e) => setFromUnit(e.target.value)}>
+              <option value="m">m</option>
+              <option value="cm">cm</option>
+              <option value="mm">mm</option>
+              <option value="km">km</option>
+              <option value="inch">inch</option>
+              <option value="foot">foot</option>
+              <option value="yard">yard</option>
+              <option value="mile">mile</option>
+            </select><br/>
+          <label for="toUnits">Unit to convert to</label>
+            <select id="toUnits" value={toUnit} onChange={(e) => setToUnit(e.target.value)}>
+              <option value="km">km</option>
+              <option value="cm">cm</option>
+              <option value="m">m</option>
+              <option value="mm">mm</option>
+              <option value="inch">inch</option>
+              <option value="foot">foot</option>
+              <option value="yard">yard</option>
+              <option value="mile">mile</option>
+            </select><br/><br/>
+          <button type="submit">Convert</button>
+        </form>
+    )
+}
